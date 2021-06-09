27bcfac59c4a170fb21e05ff38e1dbca63dbf829
Cleonildo Soares Guimaraes Junior <cleonildo.junior@b2wdigital.com>
2021-06-09 00:19:20 -0300

    First commit

---
+++ app/Models/Note.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    public $timestamps = false;
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+Route::get("/ping", function(Request $request){
+    return ['chave' => 'valor', 'funcionando' => true];
+});
+
+
+// Route::get('/notes', );
+
+// Route::get('/note/{id}');
+
+// Route::post('/note', );
+
+// Route::put('/note/{id}');
+
+// Route::put('/note/{id}');
